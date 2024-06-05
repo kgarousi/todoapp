f9236242eb787f7e1cf483aae0d47b9239e195ba
kgarousi <43246948+kgarousi@users.noreply.github.com>
2024-06-04 19:32:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { render } = require("ejs");
+const express = require("express");
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get('/',(req, res) => {
+    res.render('todo.ejs');
+    });
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(3000, () => console.log("Server Up and running"));
